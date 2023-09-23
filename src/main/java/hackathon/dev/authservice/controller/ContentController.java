ecbed7f215b810e7f277a4387bab53e56cde6a4f
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-09-23 19:57:29 +0630

    Fix

--- src/main/java/hackathon/dev/authservice/controller/ContentController.java
+++ src/main/java/hackathon/dev/authservice/controller/ContentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/contents")
 @AllArgsConstructor
+@CrossOrigin
 public class ContentController {
 
     private final ContentService contentService;
