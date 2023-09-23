6c976936c28d98a46f2201f2d166650000b81377
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-09-23 21:28:26 +0630

    CORS ERROR

--- src/main/java/hackathon/dev/authservice/controller/TierController.java
+++ src/main/java/hackathon/dev/authservice/controller/TierController.java
@@ -6,15 +6,13 @@ import hackathon.dev.authservice.service.TierService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/tiers")
+@CrossOrigin
 @AllArgsConstructor
 public class TierController {
 
