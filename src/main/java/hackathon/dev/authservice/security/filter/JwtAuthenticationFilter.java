7a9a8e939708a04618fd43c42e5dd8a6250a7e86
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-09-23 14:28:27 +0630

    Add feature

--- src/main/java/hackathon/dev/authservice/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/hackathon/dev/authservice/security/filter/JwtAuthenticationFilter.java
@@ -8,11 +8,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,12 +24,17 @@ import java.io.IOException;
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private  final JwtUtilities jwtUtilities;
+    private final JwtUtilities jwtUtilities;
     private final CustomUserDetailsService customUserDetailsService;
+    private final RestTemplate restTemplate;
+
+    private static final String API_URL = "http://10.10.1.63:3000/api/v1/auth/check";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
         String token = jwtUtilities.getToken(request);
+
         if (token != null && jwtUtilities.validateToken(token)) {
 
             String email = jwtUtilities.extractUsername(token);
@@ -46,5 +53,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request,response);
+    }
+
+    private boolean isValidClient(String token) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + token);
+
+        HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(API_URL, HttpMethod.GET, requestEntity, String.class);
+
+        return responseEntity.getStatusCode().value() == 200;
     }
 }
